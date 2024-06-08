f5db32e1cea60fe6bcb860476ca37f40576f5888
Luca Figari <luca.figari@uc.cl>
2024-06-08 13:39:02 +0100

    change in data scraping

--- main.py
+++ main.py
@@ -12,28 +12,33 @@ import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from typing import List, Dict
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+jsonExcel = {
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
 
 app = FastAPI()
 email = "lucafigarispiniello@gmail.com"
 
 @app.post("/scrape/")
 async def scrape_blog(category: str, webhook_url: str):
+
     try:
-        # data = scrape_blog_posts(category)
-        data =[
-            {
-                "title": "title1",
-                "time": "time1",
-                "category": "category1",
-                "author": "author1"
-            },
-            {
-                "title": "title2",
-                "time": "time2",
-                "category": "category2",
-                "author": "author2"
-            }
-        ]
+        data = scrape_blog_posts(category)
         save_to_google_sheets(data)
         notify_webhook(webhook_url, email)
         
@@ -84,10 +89,9 @@ def scrape_blog_posts(category):
     
     articles_section = category_soup.find('div', class_='ArticlesPagination_articlesGridNormal__NuwYU')
     
-    # find all divs with class BlogCategorySection_articleDesc3Col__6tL_x inside one level of articles_section
+
     articles = [div for div in articles_section.find_all('div', class_='ArticlesPagination_articleNormal__TZRAC', recursive=False)]
     
-    # articles = articles_section.find_all('div', class_='BlogCategorySection_articleDesc3Col__6tL_x')
     
     all_blog_posts = []
 
@@ -127,22 +131,6 @@ def scrape_blog_posts(category):
     
     return all_blog_posts
 
-
-jsonExcel = {
-  "type": "service_account",
-  "project_id": "xepelin-425714",
-  "private_key_id": "f3ce45bfc539c3a24126d7c67f434528e0fb41cd",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCFxJ73r0gp5j6P\nwS5l1q645e1JVuMt7+WzySns5cs7tdkMFW2obEfSz2h9kExypEm572hnQTp6GpMa\nMPAGnzcZPNVaw2hyGB+Zhm8L9W/vMYhbt48jQ8pGv0Fg3ktk9jM2aAJ/YyN4Rn4g\nYsIuAw5qcI6CRkaG7IcVzfVF7tB2sgCAiwZaf0QeSnXppMNgB7mhO8AjobVHDUHr\nlkg3v7OLQWndl2tPCgE0xA6Wjk2c9TZZhkCU+s9Y/BJZiPYdkHH3kZjh2nj3sonT\nMnqbqBjt0qYR5DALiUlu88RREhyeX5FKGixUFVDpEex0xWiX8/3px5xnPZcUtrLQ\nk/FGun7TAgMBAAECggEAMBdv1XlZduLVpCYeMbO8jjHvnLnVpBrG2NueLJKy/c0T\nctIzYeVH7yTtGpNpwJ+K+AN35ANh7CsorrZgXOkZzIN/6wkswDQnDDF2M/Tx4KtM\nrDiyh8mj68pvzU0uCuauo8VB/J1eT6v8RVVsHVGw+Zhsy65LRc+8gxzQLOu+W6V/\nE3k6D9yI6Y8u+S4cOrSQhnJCda6uo0u7f4kyCh9ksV8vGrIQIEJUkayx9I/rJYep\n+R0j2dxPVGccHx7SyFgw1yiqdu5g7jgEnGjgWWz9YfSGVaiCq6iX9xXA8letMiTR\nmgisN+0bt6emFevmYVhOv0BbqYtCHfROwIxNYNczkQKBgQC6SbQAgwnq2YW0DOfe\nPlTdODkbNMIG5E1QJFNcFABIcwDhXZaiWrtK8+7NZ98G0DLumpKVsrOos2pFFbXr\nxekA5bZkqMfgwXioOwH4cAklBkTrImZPvTlI9thc6tcLahVHKQvhIuFZVXHQJ34t\ngJAkh7NFjNx5meSmZCy9J430QwKBgQC304osSYD0ho/IofvF2OulNaL7mCxpvsS3\nbIpcNIIMYUtrncFr0eXdYlgEA5bcN+hwUaQ0lA7Hw6UgEkkBNr0Aam3dx0tNLQHu\nzUmrX5GwPf9/E8WxBM/mZW7PZPaua4Mnv3NvWbc47YlJHrc1pS8ekmxFqlb+E7uD\nC6+l47lqMQKBgGoXx5fzCRbjQy5Dm1oLDbHfb0Z7SXU7WHyn84GhMngQZxPyhPN5\n0Oji+8GnwnDS6e7RwWHYIFGXvJITx0O7tvN33+R76zmpddn5oSmoRMz9QQrY0IPh\nNFrFmntwk4BArlWUnttdThHeg68UjtvDOFRVpFeb3YSzjHDm5EPl3waZAoGBAJm/\nxotwFvluKohmYwyBd+ZATEceadcwBZxcngSsrjDol4o08ffaIOXfQTpqPh2GbVS0\nFCEdzJbsXgnLAWCQhdf5LFcYPzUXdcxjy0AYuOOtlyqUQP7jxXcwU1QHYANWTOZL\nzKLPaN2mXvLXS+kEdbqeQQyrggMUQftDJPAc3ZGhAoGASCny2lfX6MyY0Zg+I0WZ\nHN93EvNzciqTyIJxGiNYLWgk3xNhUZj7gNtfUeHhJQp5SVKlLZHFjmGdcIw8Mjgl\nSFruBE3c2wdK0AuRv1+QsIOxRTUjTipIdrPfjJDiVUCMSA3OkYss7lN1zeaaNACk\nuvsmNfHjm9FbVgFekKRAvPI=\n-----END PRIVATE KEY-----\n",
-  "client_email": "xepelin-177@xepelin-425714.iam.gserviceaccount.com",
-  "client_id": "100549137237467015161",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/xepelin-177%40xepelin-425714.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
-
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1puAnASoMA-5M1jO-kbaCVxGiYg9wu8Zvk98S3KTfET0"
 worksheet_name = "info"
 
 def save_to_google_sheets(data: List[Dict[str, str]]):
@@ -154,7 +142,7 @@ def save_to_google_sheets(data: List[Dict[str, str]]):
     client = gspread.authorize(creds)
 
     print("Connected to Google Sheets")
-    sheet = client.open_by_url(spreadsheet_url)
+    sheet = client.open_by_url(os.getenv("SPREADSHEET_URL"))
 
     worksheet = sheet.worksheet(worksheet_name)
     
@@ -170,7 +158,7 @@ def save_to_google_sheets(data: List[Dict[str, str]]):
 def notify_webhook(webhook_url: str, email: str):
     payload = {
         "email": email,
-        "link": spreadsheet_url
+        "link": os.getenv("SPREADSHEET_URL")
     }
     headers = {
         'Content-Type': 'application/json'
